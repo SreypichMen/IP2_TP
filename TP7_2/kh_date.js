4600b3d8d41e8dc1da784a9830aa7cd0dee3c3dc
SreypichMen <92456785+SreypichMen@users.noreply.github.com>
2022-03-06 15:30:03 +0700

    Update kh_date.js

--- TP7_2/kh_date.js
+++ TP7_2/kh_date.js
@@ -5,10 +5,13 @@ const app = express();
 var path = require('path');
 var public = path.join(__dirname, '');
 
+
 app.get('script', function(req, res) {
     res.sendFile(path.join(public, 'script.js'));
 });
+
 app.use('/', express.static(public));
+
 
 app.get("/", function(req, res) {
     readFile('kh_date.html', 'utf-8', (err, html) => {
@@ -18,6 +21,8 @@ app.get("/", function(req, res) {
 
     })
 });
+
+
 app.listen(3000, () => {
     console.log('Your app is available at http://localhost:3000');
 });
